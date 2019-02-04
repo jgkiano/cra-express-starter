14553c8c513a51875b927845511161d01dbd6294
julezKiano <jgkiano@gmail.com>
2019-02-04 22:24:44 +0300

    make eslint happy

--- client/src/__tests__/unit/pages/home.test.js
+++ client/src/__tests__/unit/pages/home.test.js
@@ -1,21 +1,21 @@
-import React from "react";
-import { mount } from "enzyme";
-import Root from "Root";
-import Home from "pages/home";
+import React from 'react';
+import { mount } from 'enzyme';
+import Root from 'Root';
+import Home from 'pages/home';
 
-describe("home component test", () => {
-    let wrap;
-    beforeEach(() => {
-        wrap = mount(
-            <Root>
-                <Home />
-            </Root>
-        );
-    });
-    afterEach(() => {
-        wrap.unmount();
-    });
-    it("displays hello world message", () => {
-        expect(wrap.find("div").text()).toEqual("Hello World");
-    });
+describe('home component test', () => {
+  let wrap;
+  beforeEach(() => {
+    wrap = mount(
+      <Root>
+        <Home />
+      </Root>,
+    );
+  });
+  afterEach(() => {
+    wrap.unmount();
+  });
+  it('displays hello world message', () => {
+    expect(wrap.find('div').text()).toEqual('Hello World');
+  });
 });
