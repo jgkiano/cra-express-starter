f6874ea191b38c31469dc54cfec07cde7dec4ae5
julezKiano <jgkiano@gmail.com>
2019-02-04 22:17:38 +0300

    added eslint

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
-const manifest = require("./manifest.json");
-const express = require("express");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const manifest = require('./manifest.json');
+
 const PORT = process.env.PORT || manifest.port;
 const ENVIRONMENT = process.env.ENVIRONMENT || manifest.environment;
 const app = express();
@@ -10,5 +11,5 @@ app.use(cors());
 app.use(morgan(ENVIRONMENT));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get("/", (req, res) => res.json({ message: "API live! 🕺" }));
+app.get('/', (req, res) => res.json({ message: 'API live! 🕺' }));
 app.listen(PORT, () => console.log(`server listening on port: ${PORT}`));
